2facda4493ae82c771642dd39b3fc72c141d3509
jaybirdddd <gajay@ucsc.edu>
2025-05-23 19:09:21 -0700

    Adding full functionality for classes

--- Assets/Scripts/UI/ClassesButtons.cs
+++ Assets/Scripts/UI/ClassesButtons.cs
@@ -5,6 +5,7 @@ public class ClassesButtons : MonoBehaviour
 {
     public string className;
     public GameObject buttonsUI;
+    public PlayerSpriteManager spriteManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,18 +16,23 @@ public class ClassesButtons : MonoBehaviour
     {
         buttonsUI.SetActive(false);
         this.className = "mage";
+        spriteManager.setSprite(0);
         Debug.Log("Player selected mage class.");
     }
     public void WarlockClass()
     {
         buttonsUI.SetActive(false);
         this.className = "warlock";
+        spriteManager.setSprite(1);
+
         Debug.Log("Player selected warlock class.");
 
     }
     public void BattlemageClass()
     {
         buttonsUI.SetActive(false);
+        spriteManager.setSprite(2);
+
         this.className = "battlemage";
         Debug.Log("Player selected battlemage class.");
 
